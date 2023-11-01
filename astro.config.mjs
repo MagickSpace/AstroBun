7b45e91a32de4beb7681bf3ae6f152b810649a15
MagickSpace <146006257+MagickSpace@users.noreply.github.com>
2023-11-01 14:06:23 +0000

    AstroBun i18next Starter

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import astroI18next from "astro-i18next";
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    devOverlay: true
+  },
+  integrations: [
+    react(),
+    astroI18next()
+  ]
+});
