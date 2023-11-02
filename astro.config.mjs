fb54c73afca778fb981abf30becba7e1e1897d3e
MagickSpace <146006257+MagickSpace@users.noreply.github.com>
2023-11-02 11:32:43 +0000

    +++

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,31 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     devOverlay: true
   },
+  site: 'https://example.com',
   integrations: [
-    react(),
-    astroI18next()
+    react(), 
+    astroI18next(), 
+    tailwind({
+      applyBaseStyles: false,
+    }), 
+    sitemap({
+      i18n: {
+        defaultLocale: 'uk',
+        locales: {
+          uk: 'uk-UA',
+          en: 'en-US', 
+          ru: 'ru-UA',
+        },
+      },
+    }),
   ]
 });
